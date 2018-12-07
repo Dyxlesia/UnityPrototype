12640ed4ea69006a402eca27deb72830eaa1c1dd
Dyxlesia <43185384+Dyxlesia@users.noreply.github.com>
2018-12-07 11:28:55 -0800

    Did some shit

---
+++ PuzzleCrawler_Prototype/Assets/Scripts/Other/Splash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > 3)
+        {
+            gameObject.transform.position = new Vector3(0, 0, -10);
+
+            if (timer > 6)
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+	}
+}
